2e44808610ab77f1800a86b5c8da89ac5ffaeaf3
Farhan akhtar <104262111+Farhan-007@users.noreply.github.com>
2024-10-12 02:28:10 +0530

    Update team.tsx

--- app/about/team.tsx
+++ app/about/team.tsx
@@ -19,7 +19,7 @@ function Team() {
                         {myTeam.map((i, idx) => {
                             return (
                                 <div key={idx} className="text-center text-gray-500 dark:text-gray-400">
-                                    <img className="mx-auto mb-4 w-36 h-36 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Avatar" />
+                                    <img className="mx-auto mb-4 w-36 h-36 rounded-full" src={i.img} alt={i.title} />
                                     <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                                         <a href="https://www.google.com/">{i.title}</a>
                                     </h3>
